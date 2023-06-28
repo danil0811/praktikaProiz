8d0d07ecaa7ddf4eb9e21d1f4e78c28456b0dc80
danil0811 <danil_glushkov_2003@inbox.ru>
2023-06-28 19:17:16 +0700

    .

--- app/src/main/java/com/example/praktika/MainActivity.java
+++ app/src/main/java/com/example/praktika/MainActivity.java
@@ -5,23 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
-import com.example.praktika.Data;
 
+import com.example.praktika.Data;
+import com.example.praktika.R;
+import com.example.praktika.activity_chart;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnFailureListener;
 import androidx.annotation.NonNull;
 
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
-    private EditText editTextMaterial;
+    private Spinner spinnerMaterial;
     private EditText editTextMaterialUsage;
     private EditText editTextEnergyUsage;
     private Button buttonSave;
@@ -34,23 +37,27 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main); // Обновленное имя файла разметки
 
-        editTextMaterial = findViewById(R.id.editTextMaterial);
+        spinnerMaterial = findViewById(R.id.spinnerMaterial);
         editTextMaterialUsage = findViewById(R.id.editTextMaterialUsage);
         editTextEnergyUsage = findViewById(R.id.editTextEnergyUsage);
         buttonSave = findViewById(R.id.buttonSave);
 
+        // Настройка адаптера для спиннера
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.materials, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMaterial.setAdapter(adapter);
+
         // Получаем ссылку на узел в базе данных, где будем хранить данные
         databaseReference = FirebaseDatabase.getInstance().getReference().child("data");
 
-        buttonSave.setOnClickListener(new View.OnClickListener()
-        {
+        buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveData();
             }
         });
 
-        Button buttonNext = findViewById(R.id.buttonNext);
+        buttonNext = findViewById(R.id.buttonNext);
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void saveData() {
-        String material = editTextMaterial.getText().toString().trim();
+        String material = spinnerMaterial.getSelectedItem().toString();
         String materialUsage = editTextMaterialUsage.getText().toString().trim();
         String energyUsage = editTextEnergyUsage.getText().toString().trim();
 
